25d4fd1c07a5252d69c3b235d09f37a3fdbb1ad9
Isabel Sopha <i.sopha94@gmail.com>
2023-03-13 18:30:56 -0700

    brb crying

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # cookie-stand
 Lab 06 for Code 201
+
+## Problem Domain
+
+Your friend Pat has come up with a business idea by combining his love for sweets and his passion for healthy ecosystems. Pat has developed a recipe for a coffee-time confection called Salmon Cookies. These cookies made into the shape of a salmon and are suitable for both humans and salmon to enjoy.
+
+Pat needs some help with the branding of the business, as well as some help with internal data management for the company, and has enlisted your assistance because of your extensive and proven work in developing web applications.
+
+Pat’s Salmon Cookies, soon with franchises internationally, needs a proof of concept application to calculate the number of cookies each location must make every day so that it can manage its supplies inventory and baking schedule. The number of cookies to make depends on the hours of operation (6:00 AM to 8:00 PM for all locations) and a few factors unique to each location:
+
+The minimum number of customers per hour.
+The maximum number of customers per hour.
+The average number of cookies purchased per customer.
+Because we are early in the life of this business, we will need to build an application that is adaptable. Pat will need to be able to add and remove locations from the daily projections report, and Pat will also need to be able to easily modify the input numbers for each location based on day of the week, special events, and other factors. Pat would like to see these numbers with nice formatting in a web application.
+
+Pat needs you to take a leading role in doing the design work and construction of a public-facing page, too. They already have a logo image picked out (below), but Pat is requesting your assistance in the design of the documents, color scheme, fonts, and any additional images for the public facing site.
+
+![Salmon](./img/salmon.png)
+
+So, in addition to building an application that calculates daily sales projections for each location (on a page called sales.html), you also need to create a public-facing page (on the homepage index.html) that is colorful, eye-catching, readable, useful, and informative.
+
+You’ve got a lot to do. Plan your work, and work your plan.
---
+++ app.js
@@ -0,0 +1,255 @@
+'use strict';
+
+const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// SEATTLE BELOW:
+
+let seattle = {
+    location: 'Seattle',
+    minCust: 23,
+    maxCust: 65,
+    avgSale: 6.3,
+    customersPerHour: [],
+    cookiesPerHour: [],
+    totalCookies: 0,
+    getCustomersPerHour: function () {
+        for (let i = 0; i < hours.length; i++) {
+        this.customersPerHour.push(random(this.minCust, this.maxCust));
+        }
+    },
+
+    getCookiesPerHour: function () {
+        this.getCustomersPerHour();
+        for (let i = 0; i < hours.length; i++) {
+            let hour1 = Math.ceil(this.customersPerHour[i] * this.avgSale);
+            this.cookiesPerHour.push(hour1);
+            this.totalCookies += hour1;
+        }
+    },
+
+    render: function () {
+        this.getCookiesPerHour()
+        let seattleList = document.getElementById('seattle');
+        for (let i = 0; i < hours.length; i++) {
+            let liElement = document.createElement('li');
+            liElement.textContent = `${hours[i]}: ${this.cookiesPerHour[i]} cookies.`;
+            seattleList.appendChild(liElement);
+        }
+
+        let liElement = document.createElement('li');
+        liElement.textContent = `Total: ${this.totalCookies} cookies.`;
+        seattleList.appendChild(liElement);
+    },
+
+};
+
+// // target parent element
+// // create element and give it content
+// // append to parent
+
+// // Seattle
+seattle.render();
+console.log(seattle.cookiesPerHour);
+
+// // LI Elements
+let seattleElement = document.getElementById('seaH2');
+let h2Element1 = document.createElement('h2');
+h2Element1.textContent = seattle.location;
+seattleElement.appendChild(h2Element1);
+
+// TOKYO BELOW:
+
+let tokyo = {
+    location: 'Toyko',
+    minCust: 3,
+    maxCust: 24,
+    avgSale: 1.2,
+    customersPerHour: [],
+    cookiesPerHour: [],
+    totalCookies: 0,
+    getCustomersPerHour: function () {
+        for (let i = 0; i < hours.length; i++) {
+        this.customersPerHour.push(random(this.minCust, this.maxCust));
+        }
+    },
+
+    getCookiesPerHour: function () {
+        this.getCustomersPerHour();
+        for (let i = 0; i < hours.length; i++) {
+            let hour1 = Math.ceil(this.customersPerHour[i] * this.avgSale);
+            this.cookiesPerHour.push(hour1);
+            this.totalCookies += hour1;
+        }
+    },
+
+    render: function () {
+        this.getCookiesPerHour()
+        let tokyoList = document.getElementById('tokyo');
+        for (let i = 0; i < hours.length; i++) {
+            let liElement = document.createElement('li');
+            liElement.textContent = `${hours[i]}: ${this.cookiesPerHour[i]} cookies.`;
+            tokyoList.appendChild(liElement);
+        }
+    
+        let liElement = document.createElement('li');
+        liElement.textContent = `Total: ${this.totalCookies} cookies.`;
+        tokyoList.appendChild(liElement);
+    },
+
+};
+
+tokyo.render();
+console.log(tokyo.cookiesPerHour);
+
+let tokyoElement = document.getElementById('tokyoH2');
+let h2Element2 = document.createElement('h2');
+h2Element2.textContent = tokyo.location;
+tokyoElement.appendChild(h2Element2);
+
+// // DUBAI BELOW:
+
+let dubai = {
+    location: "Dubai",
+    minCust: 11,
+    maxCust: 38,
+    avgSale: 3.7,
+    customersPerHour: [],
+    cookiesPerHour: [],
+    totalCookies: 0,
+    getCustomersPerHour: function () {
+        for (let i = 0; i < hours.length; i++) {
+        this.customersPerHour.push(random(this.minCust, this.maxCust));
+        }
+    },
+
+    getCookiesPerHour: function () {
+        this.getCustomersPerHour();
+        for (let i = 0; i < hours.length; i++) {
+            let hour1 = Math.ceil(this.customersPerHour[i] * this.avgSale);
+            this.cookiesPerHour.push(hour1);
+            this.totalCookies += hour1;
+        }
+    },
+
+    render: function () {
+        this.getCookiesPerHour()
+        let dubaiList = document.getElementById('dubai');
+        for (let i = 0; i < hours.length; i++) {
+            let liElement = document.createElement('li');
+            liElement.textContent = `${hours[i]}: ${this.cookiesPerHour[i]} cookies.`;
+            dubaiList.appendChild(liElement);
+        }
+
+        let liElement = document.createElement('li');
+        liElement.textContent = `Total: ${this.totalCookies} cookies.`;
+        dubaiList.appendChild(liElement);
+    },
+};
+
+dubai.render();
+console.log(dubai.cookiesPerHour);
+
+let dubaiElement = document.getElementById('dubaiH2');
+let h2Element3 = document.createElement('h2');
+h2Element3.textContent = dubai.location;
+dubaiElement.appendChild(h2Element3);
+
+// // PARIS BELOW:
+
+let paris = {
+    location: "Paris",
+    minCust: 11,
+    maxCust: 38,
+    avgSale: 3.7,
+    customersPerHour: [],
+    cookiesPerHour: [],
+    totalCookies: 0,
+    getCustomersPerHour: function () {
+        for (let i = 0; i < hours.length; i++) {
+        this.customersPerHour.push(random(this.minCust, this.maxCust));
+        }
+    },
+
+    getCookiesPerHour: function () {
+        this.getCustomersPerHour();
+        for (let i = 0; i < hours.length; i++) {
+            let hour1 = Math.ceil(this.customersPerHour[i] * this.avgSale);
+            this.cookiesPerHour.push(hour1);
+            this.totalCookies += hour1;
+        }
+    },
+
+    render: function () {
+        this.getCookiesPerHour()
+        let parisList = document.getElementById('paris');
+        for (let i = 0; i < hours.length; i++) {
+            let liElement = document.createElement('li');
+            liElement.textContent = `${hours[i]}: ${this.cookiesPerHour[i]} cookies.`;
+            parisList.appendChild(liElement);
+        }
+
+        let liElement = document.createElement('li');
+        liElement.textContent = `Total: ${this.totalCookies} cookies.`;
+        parisList.appendChild(liElement);
+    },
+}
+
+paris.render();
+console.log(paris.cookiesPerHour);
+
+let parisElement = document.getElementById('parisH2');
+let h2Element4 = document.createElement('h2');
+h2Element4.textContent = paris.location;
+parisElement.appendChild(h2Element4);
+
+// // LIMA BELOW:
+
+let lima = {
+    location: "Lima",
+    minCust: 11,
+    maxCust: 38,
+    avgSale: 3.7,
+    customersPerHour: [],
+    cookiesPerHour: [],
+    totalCookies: 0,
+    getCustomersPerHour: function () {
+        for (let i = 0; i < hours.length; i++) {
+        this.customersPerHour.push(random(this.minCust, this.maxCust));
+        }
+    },
+
+    getCookiesPerHour: function () {
+        this.getCustomersPerHour();
+        for (let i = 0; i < hours.length; i++) {
+            let hour1 = Math.ceil(this.customersPerHour[i] * this.avgSale);
+            this.cookiesPerHour.push(hour1);
+            this.totalCookies += hour1;
+        }
+    },
+
+    render: function () {
+        this.getCookiesPerHour()
+        let limaList = document.getElementById('lima');
+        for (let i = 0; i < hours.length; i++) {
+            let liElement = document.createElement('li');
+            liElement.textContent = `${hours[i]}: ${this.cookiesPerHour[i]} cookies.`;
+            limaList.appendChild(liElement);
+        }
+
+        let liElement = document.createElement('li');
+        liElement.textContent = `Total: ${this.totalCookies} cookies.`;
+        limaList.appendChild(liElement);
+    },
+}
+
+lima.render();
+console.log(lima.cookiesPerHour);
+
+let limaElement = document.getElementById('limaH2');
+let h2Element5 = document.createElement('h2');
+h2Element5.textContent = lima.location;
+limaElement.appendChild(h2Element5);
