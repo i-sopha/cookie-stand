251e575b25d553aba41699d8091eb9632dbab277
Isabel Sopha <i.sopha94@gmail.com>
2023-03-15 21:01:50 -0700

    added to files for lab 9

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
